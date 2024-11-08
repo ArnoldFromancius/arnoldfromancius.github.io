51442175530b62f469cacd3fc20d628cf23ec71b
Arnold Fromancius. Tembo <40474427+ArnoldFromancius@users.noreply.github.com>
2024-11-09 01:27:26 +0200

    Update main.js

--- web/assets/scripts/main.js
+++ web/assets/scripts/main.js
@@ -4,3 +4,14 @@
    document.write("<style>body{zoom: 150%;}</style>");
    $("#BANNER").hide();
  }
+
+
+var mobile = (/iphone|ipod|android|blackberry|mini|windows\sce|palm/i.test(navigator.userAgent.toLowerCase()));  
+    if (mobile) { 
+        alert("MOBILE DEVICE!!");
+        $('.navWrap').css('display', 'none'); // OR you can use $('.navWrap').hide();
+    } 
+    else 
+    { 
+       alert("NOT A MOBILE DEVICE!!"); 
+    }
