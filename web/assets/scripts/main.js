cbafd0ae7ff4fab64471c0dd56e1f44730e493de
Arnold Fromancius. Tembo <40474427+ArnoldFromancius@users.noreply.github.com>
2024-11-09 01:40:31 +0200

    Update main.js

--- web/assets/scripts/main.js
+++ web/assets/scripts/main.js
@@ -6,12 +6,5 @@
  }
 
 
-var mobile = (/iphone|ipod|android|blackberry|mini|windows\sce|palm/i.test(navigator.userAgent.toLowerCase()));  
-    if (mobile) { 
-        alert("MOBILE DEVICE!!");
-        $('.navWrap').css('display', 'none'); // OR you can use $('.navWrap').hide();
-    } 
-    else 
-    { 
-       alert("NOT A MOBILE DEVICE!!"); 
-    }
+     var mobile = (/iphone|ipod|android|blackberry|mini|windows\sce|palm/i.test(navigator.userAgent.toLowerCase()));  
+     if (mobile) {$('.navWrap').hide();}
