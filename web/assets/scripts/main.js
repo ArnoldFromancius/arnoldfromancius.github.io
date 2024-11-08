b8eb8bd03a9f2f9dfba0d01472a918781084759c
Arnold Fromancius. Tembo <40474427+ArnoldFromancius@users.noreply.github.com>
2024-11-09 01:42:29 +0200

    Update main.js

--- web/assets/scripts/main.js
+++ web/assets/scripts/main.js
@@ -7,4 +7,4 @@
 
 
      var mobile = (/iphone|ipod|android|blackberry|mini|windows\sce|palm/i.test(navigator.userAgent.toLowerCase()));  
-     if (mobile) {$('.navWrap').hide();}
+     if (mobile) {$('#BANNER').hide();}
