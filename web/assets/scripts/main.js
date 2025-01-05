b1ef7994ab70fba14a1b5119dac6a022d3630963
“arnoldfromancius” <arnoldfromancius@gmail.com>
2025-01-05 14:41:12 +0200

    increased width of footer.

--- web/assets/scripts/main.js
+++ web/assets/scripts/main.js
@@ -14,18 +14,31 @@ if (isMobile) {
   document.getElementById('BANNER').style.display = 'none';
 
   document.getElementByID('headerID').style.width = '100%';
-  document.getElementById('headerID').style.marginLeft = '0%';
+  document.getElementById('headerID').style.marginLeft = 'auto';
 
   document.getElementById('navbar').style.width = '100%';
-  document.getElementById('navbar').style.marginLeft = '0%';
+  document.getElementById('navbar').style.marginLeft = 'auto';
+
+  document.getElementById('mainContent').style.width = 'auto';
+  document.getElementById('mainContent').style.marginLeft = 'auto';
 } else {
   // User is accessing the page on a desktop device
   console.log("Desktop device detected");
   document.getElementById('BANNER').style.display = 'none';
 
-  document.getElementById('headerID').style.width = '100%';
-  document.getElementById('headerID').style.marginLeft='0%';
+  document.getElementById('headerID').style.width = 'auto';
+  document.getElementById('headerID').style.marginLeft='auto';
 
-  document.getElementById('navbar').style.width = '100%';
-  document.getElementById('navbar').style.marginLeft = '0%';
+  document.getElementById('navbar').style.width = 'auto';
+  document.getElementById('navbar').style.marginLeft = 'auto';
+
+  document.getElementById('mainContent').style.width = 'auto';
+  document.getElementById('mainContent').style.marginLeft = 'auto';
+
+  document.getElementById('sideBar').style.width = 'auto';
+  document.getElementById('sideBar').style.marginLeft = 'auto';
+
+  document.getElementById('Footer').style.width = 'auto';
+  document.getElementById('Footer').style.marginLeft = 'auto';
+
 }
