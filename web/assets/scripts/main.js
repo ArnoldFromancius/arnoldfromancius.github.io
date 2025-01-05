382013e54dafc8cb6045b00309c3e57aa2b84df1
“arnoldfromancius” <arnoldfromancius@gmail.com>
2025-01-05 14:22:43 +0200

    maximized header to 100% width

--- web/assets/scripts/main.js
+++ web/assets/scripts/main.js
@@ -12,12 +12,20 @@ if (isMobile) {
   // User is accessing the page on a mobile device
   console.log("Mobile device detected");
   document.getElementById('BANNER').style.display = 'none';
-  //document.getElementByTag('header').style.width = '100%';
-  //document.getElementById('navbar').style.width = '100%';
+
+  document.getElementByID('headerID').style.width = '100%';
+  document.getElementById('headerID').style.marginLeft = '0%';
+
+  document.getElementById('navbar').style.width = '100%';
+  document.getElementById('navbar').style.marginLeft = '0%';
 } else {
   // User is accessing the page on a desktop device
   console.log("Desktop device detected");
   document.getElementById('BANNER').style.display = 'none';
-  //document.getElementByTag('header').style.width = '100%';
-  //document.getElementById('navbar').style.width = '100%';
+
+  document.getElementById('headerID').style.width = '100%';
+  document.getElementById('headerID').style.marginLeft='0%';
+
+  document.getElementById('navbar').style.width = '100%';
+  document.getElementById('navbar').style.marginLeft = '0%';
 }
